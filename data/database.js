d9e63fd0a2b617b849be5ab1e870d76755aa51a3
satyavara90 <S.Varadarajan2@shell.com>
2023-07-16 11:53:29 +0530

    4

--- data/database.js
+++ data/database.js
@@ -1,9 +1,9 @@
 import { MongoClient } from 'mongodb';
 
-const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
-const dbUser = process.env.MONGODB_USERNAME;
-const dbPassword = process.env.MONGODB_PASSWORD;
-const dbName = process.env.MONGODB_DB_NAME;
+const clusterAddress = "cluster1.jiw4o60.mongodb.net";
+const dbUser = "zizusat";
+const dbPassword = "ND2UMi8jklEq9uKv"
+const dbName = "gha-demo";
 
 const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
